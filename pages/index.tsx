221cf830bb912595c5e5ab5ea634eaefc14894f2
Klendi Gocci <klendigocci@gmail.com>
2020-03-15 13:06:25 +0100

    Initial Commit

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Head from "next/head";
+
+const Home = () => (
+  <div>
+    <Head>
+      <title>Home</title>
+    </Head>
+    <div>This is the main content</div>
+  </div>
+);
+
+export default Home;
